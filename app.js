65e3aaae9efd0dc11a07fe8fc2e526b1ac017dad
vinchi777 <jude_calimbas@yahoo.com>
2013-09-26 20:56:00 +0800

    init

---
+++ app.js
@@ -0,0 +1,111 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express');
+var http = require('http');
+var path = require('path');
+
+var app = express();
+
+
+var mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/test');
+
+var CommentSchema = new mongoose.Schema({
+		author: String,
+		time_elapsed: {type: Date, default: Date.now},
+		message: String,
+	    upvotes: Number
+});
+var Comment = mongoose.model('comments', CommentSchema);
+
+var corsSettings = function(req, res, next) {
+	res.header('Access-Control-Allow-Origin', '*');
+	res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+	res.header('Access-Control-Allow-Headers', 'origin, content-type, accept');
+	// deal with OPTIONS method during a preflight request
+	if (req.method === 'OPTIONS') {
+		res.send(200);
+	} else {
+		next();
+	}
+}
+
+app.use(express.bodyParser());
+app.use(corsSettings);
+
+// all environments
+app.set('port', process.env.PORT || 9090);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.methodOverride());
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
+
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+
+function listComments(req, res){
+	Comment.find(null, null, options, function (err, docs) {
+		if (err) {
+			console.log(err);
+			res.send(500, err);
+		} else {
+			res.send(200, docs);
+		}
+	});
+}
+
+function createComment(req, res){
+	Comment.create(req.body, function (err, doc) {
+		if (err) {
+			console.log(err);
+			res.send(500, err);
+		} else {
+			res.send(200, doc);
+		}
+	});
+}
+
+function deleteComment(req, res){
+	var id = req.params.id;
+	Comment.findByIdAndRemove(id, function (err, doc) {
+		if (err) {
+			console.log(err);
+			res.send(404, err);
+		} else {
+			res.send(200, doc);
+		}
+	});
+}
+
+function updateComment(req, res){
+	var id = req.params.id;
+	var newData = {
+		upvotes: req.body.upvotes
+	};
+	Comment.findByIdAndUpdate(id, newData, function (err, doc) {
+		if (err) {
+			console.log(err);
+			res.send(404, err);
+		} else {
+			res.send(200, doc);
+		}
+	});
+}
+
+app.get('/comments', listComments);
+app.post('/comments', createComment);
+app.put('/comments/:id', updateComment);
+app.delete('/comments/:id', deleteComment);
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
---
+++ commenting-system/assets/scripts/main.js
@@ -0,0 +1,124 @@
+(function ($, BB, _) {
+
+	var App = Backbone.View.extend({
+		el: "#comments",
+		events: {
+			'click .button': 'createComment'
+		},
+		initialize: function () {
+		  this.$input_author = $("#author"),
+		  this.$input_message = $("#message"),
+		  this.listenTo(this.collection, 'add', this.createView),
+		  this.$comment_list = $("#comment_list")
+		},
+		createComment: function(){
+		   var _this = this;
+
+			var comment = new CommentModel({
+				author: this.$input_author.val(),
+				message: this.$input_message.val(),
+				time_elapsed: Date()
+			});
+
+			comment.save(null, {
+				success: function (model, resp, options) {
+					_this.collection.add(model);
+				},
+				error: function (model, xhr, options) {
+					alert('Error on save');
+				}
+			});
+		},
+		createView: function (model, collection) {
+			var view = new CommentView({ model: model});
+			this.$comment_list.append(view.render().el);
+		}
+	});
+
+	var CommentModel = Backbone.Model.extend({
+		defaults: {
+			author: '',
+			message: '',
+			time_elapsed: '',
+			upvotes: 0
+		},
+		idAttribute: '_id',
+		url: function () {
+			var location = 'http://localhost:9090/comments';
+			return this.id ? (location + '/' + this.id) : location;
+		},
+		initialize: function () {
+		}
+
+	});
+
+	var CommentCollection = Backbone.Collection.extend({
+		model: CommentModel,
+		url: 'http://localhost:9090/comments',
+		initialize: function () {
+
+		}
+	});
+
+	var CommentView = Backbone.View.extend({
+		tagName: 'li',
+		template: $('#comment-template').html(),
+		events: {
+		  'click .upvote' : 'upvoteComment',
+		  'click .delete' : 'removeComment'
+		},
+		initialize: function() {
+		  this.listenTo(this.model, 'destroy', this.removeView);
+		  this.listenTo(this.model, 'change' , this.changeVote);
+		},
+		changeVote: function(){
+			var compiledTemplate = _.template(this.template);
+			this.$el.html(compiledTemplate(this.model.toJSON()));
+		},
+		upvoteComment: function(){
+		  var newAttrs = {
+				author: this.model.get('author'),
+				message: this.model.get('message'),
+				time_elapsed: this.model.get('time_elapsed'),
+				upvotes: this.model.get('upvotes') + 1
+			}
+
+		  this.model.save(newAttrs, {
+			  wait: true,
+		  success: function (model, resp, opt) {
+			  console.log('model update success: ', model);
+		  },
+		  error: function (model, xhr, opt) {
+			  console.log('model update error: ', model);
+		  }
+		  });
+
+		},
+		removeComment: function () {
+			this.model.destroy({
+				wait: true,
+				success: function (model, resp, opt) {
+					console.log('model destroy success: ', model);
+				},
+				error: function (model, xhr, opt) {
+					console.log('model destroy error: ', model);
+				}
+			})
+		},
+		removeView: function () {
+			this.undelegateEvents();
+			this.stopListening();
+			this.remove();
+		},
+		render: function() {
+			var compiledTemplate = _.template(this.template);
+			this.$el.html(compiledTemplate(this.model.toJSON()))
+			return this;
+		}
+	});
+
+	var commentApp = new App({ collection: new CommentCollection() });
+
+	// for debugging purposes
+	window.app = commentApp;
+})(jQuery, Backbone, _)
